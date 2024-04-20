3377e3e5f4987ab867f329096403a65a7d958e29
Virjilakrum <asesenep15@gmail.com>
2024-04-20 14:29:42 +0300

    add openzeppelin-interfaces

--- launchpad/apps/EVM-contracts/TokenSale/APTokenSale.sol
+++ launchpad/apps/EVM-contracts/TokenSale/APTokenSale.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TokenSale {
     IERC20 public token;
