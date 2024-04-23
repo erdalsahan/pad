5cfa9969ae4d83a9bf73a2501bd3eb52c5eff393
Yigid BALABAN <fyb@fybx.dev>
2024-04-23 21:28:02 +0300

    + component: ProjectCard

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,142 @@
+<!-- ProjectCard component
+     2024 © Atlaspad Launchpad
+     Yigid BALABAN <fyb@fybx.dev
+-->
+<script>
+	import randomGradient from '../randomGradient';
+
+	export let name = 'Project Name';
+	
+	export let flair = 'TBA'; // Valid keys: [ private sale, token sale, tba ]
+	const flairToColorMap = {
+		'Private Sale': '#4B04F1',
+		'Token Sale': '#88F2E7',
+		'TBA': '#EAFF96'
+	};
+	$: flairColor = flairToColorMap[flair];
+
+	export let details = [
+		{ key: 'Add some', value: 'details' },
+		{ key: 'you lazy', value: 'ass' },
+		{ key: 'by the way', value: 'we got properties', monospaced: true },
+		{ key: 'mute me please', value: 'sir yes sir', muted: true }
+	];
+	export let bannerImageSource = randomGradient();
+
+	export let chainSymbol = ''; // Valid keys: [ eth, avax, mina ]
+	const imageMap = {
+		'': 'logo.svg',
+		eth: 'chain/eth.svg',
+		mina: 'chain/mina.svg',
+		avax: 'chain/avax.svg'
+	};
+	$: chainImageUrl = imageMap[chainSymbol];
+</script>
+
+<div class="card-project" style="--flair: {flairColor};">
+	<span>{flair}</span>
+	<div class="banner" style="background-image: {bannerImageSource};"></div>
+	<div class="title">
+		<h1>{name}</h1>
+		<img src={chainImageUrl} alt="Project main chain logo" />
+	</div>
+	<hr />
+	<table>
+		{#each details as detail}
+			<tr>
+				{#if detail.muted}
+					<td class="color-muted">{detail.key}</td>
+				{:else}
+					<td>{detail.key}</td>
+				{/if}
+				{#if detail.monospaced}
+					<td class="font-monospace">
+						{#if detail.muted}
+							<span class="color-muted">{detail.value}</span>
+						{:else}
+							{detail.value}
+						{/if}
+					</td>
+				{:else if detail.muted}
+					<td class="color-muted">{detail.value}</td>
+				{:else}
+					<td>{detail.value}</td>
+				{/if}
+			</tr>
+		{/each}
+	</table>
+	<button>Details</button>
+</div>
+
+<style lang="scss">
+	.card-project {
+		display: inline-flex;
+		min-width: 320px;
+		padding: 16px;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 10px;
+
+		border-radius: 16px;
+		border: 2px solid rgba(255, 255, 255, 0.16);
+		background: rgba(217, 217, 217, 0.08);
+		-webkit-backdrop-filter: blur(14px);
+		backdrop-filter: blur(14px);
+		box-shadow: 0px 0px 16px 0px var(--flair); 
+
+		.banner {
+			aspect-ratio: 1.5;
+			border-radius: 8px;
+			width: 100%;
+		}
+
+		.title {
+			width: 100%;
+
+			h1 {
+				display: inline;
+			}
+
+			img {
+				float: right;
+				width: 1.5rem;
+				height: auto;
+			}
+		}
+
+		hr {
+			width: 100%;
+		}
+
+		table {
+			width: 100%;
+			tr td {
+				font-size: 1.25rem;
+
+				&:last-of-type {
+					text-align: right;
+				}
+			}
+		}
+
+		button {
+			padding: 8px 16px;
+			align-self: stretch;
+
+			color: #fff;
+			font:
+				1rem 'Ubuntu',
+				sans-serif;
+
+			border: none;
+			border-radius: 8px;
+			background: rgba(217, 217, 217, 0.05);
+			box-shadow:
+				1.333px -1.333px 1.333px 0px rgba(174, 174, 174, 0.29) inset,
+				2.667px -2.667px 2.667px 0px rgba(255, 255, 255, 0.29) inset,
+				-1.333px 1.333px 1.333px 0px rgba(255, 255, 255, 0.29) inset,
+				-2.667px 2.667px 2.667px 0px rgba(174, 174, 174, 0.29) inset;
+		}
+	}
+</style>
