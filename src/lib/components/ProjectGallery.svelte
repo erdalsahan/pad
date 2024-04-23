eb517ea6651ba7f9bda468be9353a38d6e4b7236
Yigid BALABAN <fyb@fybx.dev>
2024-04-24 02:10:31 +0300

    enh: added SearchBar and tags

--- src/lib/components/ProjectGallery.svelte
+++ src/lib/components/ProjectGallery.svelte
@@ -6,6 +6,8 @@
 <script>
 	import { onMount } from 'svelte';
 	import ProjectCard from './ProjectCard.svelte';
+	import ImageButton from './ImageButton.svelte';
+	import SearchBar from './SearchBar.svelte';
 
 	let galleryData = [];
 	let loading = true;
@@ -15,7 +17,7 @@
 			loading = true;
 			const response = await fetch('/api/gallery.json');
 			const data = await response.json();
-            galleryData = data['data'];
+			galleryData = data['data'];
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		} finally {
@@ -23,30 +25,79 @@
 		}
 	}
 
+	async function handleSearch(userQuery) {
+		console.log('yeah, we are on it!!', userQuery);
+	}
+
 	onMount(fetchData);
 </script>
 
-{#if loading}
-	<div class="loading-animation">Loading...</div>
-{:else}
-	<div class="gallery">
-		{#each galleryData as item}
-            <ProjectCard name={item.name} flair={item.flair} details={item.details} chainSymbol={item.chain} />
-		{/each}
+<section>
+	<div class="controls">
+		<nav>
+			<ImageButton text="Upcoming" imgSource="button/upc.png" />
+			<ImageButton text="Active" imgSource="button/act.png" />
+			<ImageButton text="Completed" imgSource="button/com.png" />
+			<ImageButton text="Vesting" imgSource="button/ves.png" />
+		</nav>
+		<nav>
+			<ImageButton text="$MINA" imgSource="chain/mina.svg" />
+			<ImageButton text="$ETH" imgSource="chain/eth.svg" />
+			<ImageButton text="$AVAX" imgSource="chain/avax.svg" />
+		</nav>
+		<SearchBar onSearch={handleSearch} />
 	</div>
-{/if}
+	{#if loading}
+		<div class="loading-animation">Loading...</div>
+	{:else}
+		<div class="gallery">
+			{#each galleryData as item}
+				<ProjectCard
+					name={item.name}
+					flair={item.flair}
+					details={item.details}
+					chainSymbol={item.chain}
+				/>
+			{/each}
+		</div>
+	{/if}
+</section>
 
-<style>
-	.loading-animation {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100px;
-	}
-	.gallery {
-		display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        gap: 2rem 1rem;
+<style lang="scss">
+	section {
+		.controls {
+			display: grid;
+			grid-template-columns: 1fr auto;
+			grid-template-rows: repeat(1fr, 2);
+			grid-auto-columns: 1fr;
+			gap: 2em 0em;
+			grid-template-areas:
+				'tags chains'
+				'searchbar searchbar';
+			margin-bottom: 2rem;
+
+			nav {
+				&:first-of-type {
+					grid-area: tags;
+				}
+
+				&:last-of-type {
+					grid-area: chains;
+				}
+			}
+		}
+
+		.loading-animation {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100px;
+		}
+		.gallery {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+			gap: 2rem 1rem;
+		}
 	}
 </style>
