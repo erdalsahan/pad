a69ef4eadc53f81d63ffe1c36e603091c95f3be8
Yigid BALABAN <fyb@fybx.dev>
2024-04-24 14:35:54 +0300

    enh: checks and flexibility
    checks if the given el is a real HTMLElement before attempting an assignment & can assign to any style attribute.

--- src/lib/utils/calculatePadding.js
+++ src/lib/utils/calculatePadding.js
@@ -2,10 +2,10 @@
  *	2024 © Atlaspad Launchpad
  *  Yigid BALABAN <fyb@fybx.dev
  */
-export default () => {
+export default (el, attr) => {
 	// the magic unitless value 0.14 is the m for y = mx + n
 	// that points (320, 16) and (1920, 240) pass through.
 	let value = 0.14 * (window.innerWidth - 320) + 16;
-	document.body.style.paddingInline = `${value}px`;
+	if (el instanceof HTMLElement) el.style[attr] = `${value}px`;
 	return window.innerWidth - value * 2;
 };
