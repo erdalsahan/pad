39fe56a32f2f2b420aa9b41c59f437d6ea2e3d15
Yigid BALABAN <fyb@fybx.dev>
2024-04-24 17:54:10 +0300

    feat: animated tile background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,25 @@
 	import calculatePadding from '../lib/utils/calculatePadding';
 	import Footer from '../lib/partials/footer.svelte';
 
+	let columns = 0;
+	let rows = 0;
+	let total = 1;
+
+	const getGridSize = () => {
+		columns = Math.floor(document.body.clientWidth / 50);
+		rows = Math.floor(document.body.clientHeight / 50);
+		total = rows * columns;
+	};
+
 	let main;
 	onMount(() => {
-		calculatePadding(main, 'paddingInline');
-
-		window.onresize = () => {
+		getGridSize();
+		window.addEventListener('resize', getGridSize);
+		window.addEventListener('resize', () => {
 			calculatePadding(main, 'paddingInline');
-		};
+		});
+
+		calculatePadding(main, 'paddingInline');
 	});
 </script>
 
@@ -24,6 +36,12 @@
 	/>
 </svelte:head>
 
+<div id="grid">
+	{#each Array(total) as _, i}
+		<div class="grid-item" id={i}></div>
+	{/each}
+</div>
+
 <Header />
 
 <main bind:this={main}>
@@ -33,6 +51,8 @@
 <Footer />
 
 <style lang="scss">
+	$square_size: 80px;
+
 	:global(*) {
 		margin: 0;
 		padding: 0;
@@ -73,5 +93,69 @@
 
 	:global(.color-muted) {
 		color: #ccc;
+	}
+
+	#grid {
+		z-index: -1;
+		position: fixed;
+		top: 0;
+		width: 100vw;
+		height: 100svh;
+		height: 100vh;
+
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax($square_size, 1fr));
+		grid-template-rows: repeat(auto-fit, minmax($square_size, 1fr));
+		justify-content: center;
+
+		background: linear-gradient(
+			45deg,
+			#4B04F140,
+			#35ADA140,
+			#52FF0040,
+			#88F2E740,
+			#79996040,
+			#EAFF9640,
+			#FF5B9640
+		);
+		background-size: 200% 200%;
+		animation: flowingGradient 8s ease infinite;
+
+		@keyframes flowingGradient {
+			0% {
+				background-position: 0% 50%;
+			}
+			50% {
+				background-position: 100% 50%;
+			}
+			100% {
+				background-position: 0% 50%;
+			}
+		}
+
+		.grid-item {
+			min-width: 100%;
+			min-height: 100%;
+			background-color: transparent;
+			cursor: pointer;
+			position: relative;
+			&:after {
+				content: '';
+				position: absolute;
+				background-color: rgb(15, 15, 15);
+				inset: .5px;
+			}
+		}
+
+		&:after {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+
+			background-image: url('bg/reveal.png');
+		}
 	}
 </style>
