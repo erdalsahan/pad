74b857df20e935a06657a47f5b243a78a62a8bcc
Virjilakrum <asesenep15@gmail.com>
2024-04-26 16:43:21 +0300

    add-component-Provider.svelte

---
+++ src/lib/components/Provider.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher, createContext } from 'svelte';
+
+
+  const mainContext = createContext();
+
+
+  const mainContextValue = buildMainContextValue();
+
+  const dispatch = createEventDispatcher();
+
+  export let children;
+</script>
+
+<mainContext.Provider value={mainContextValue}>
+  {children}
+</mainContext.Provider>
+
+<style>
+  /* :p */
+</style>
