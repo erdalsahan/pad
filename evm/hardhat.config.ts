f095c3228274616df5e568dfc113343032ac8038
Yigid BALABAN <fyb@fybx.dev>
2024-04-30 01:05:28 +0300

    deps: hardhat & configuration (chain: 1337)

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+	solidity: '0.8.24',
+	networks: {
+		hardhat: {
+			chainId: 1337
+		},
+
+		running: {
+			url: 'http://localhost:8545',
+			chainId: 1337
+		}
+	}
 };
 
 export default config;
