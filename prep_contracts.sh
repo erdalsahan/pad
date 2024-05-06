650348ca1487e4b66b755baf720c6b03c781e729
Yigid BALABAN <fyb@fybx.dev>
2024-05-06 14:27:41 +0300

    enh: don't try to copy artifacts if compilation fails

--- prep_contracts.sh
+++ prep_contracts.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# change this part according to your chain
 CHAIN="31337"
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -9,6 +8,19 @@ chain_dir="$script_dir/evm/ignition/deployments/chain-$CHAIN"
 artifacts_dir="$chain_dir/artifacts"
 target_dir="$script_dir/src/lib/abi"
 
+move_over() {
+    cd "$script_dir"
+
+    for file in $files; do
+        new_filename="${file#*#}"
+        cp -v "$artifacts_dir/$file" "$target_dir/$new_filename"
+    done
+
+    cp -v "$script_dir/evm/artifacts/contracts/Campaign/APCampaign.sol/APCampaign.json" "$target_dir/"
+
+    cp -v "$chain_dir/deployed_addresses.json" "$target_dir/"
+}
+
 mkdir -p $target_dir
 
 files="AtlaspadDemoModule#APLock.json AtlaspadDemoModule#APPool.json AtlaspadDemoModule#APPoolManager.json AtlaspadDemoModule#APToken.json AtlaspadDemoModule#APCampaignManager.json"
@@ -16,13 +28,6 @@ files="AtlaspadDemoModule#APLock.json AtlaspadDemoModule#APPool.json AtlaspadDem
 cd "$script_dir/evm"
 pnpm run hardhat ignition deploy $script_dir/evm/ignition/modules/AtlaspadDemo.ts --network localhost
 
-cd "$script_dir"
-
-for file in $files; do
-    new_filename="${file#*#}"
-    cp -v "$artifacts_dir/$file" "$target_dir/$new_filename"
-done
-
-cp -v "$script_dir/evm/artifacts/contracts/Campaign/APCampaign.sol/APCampaign.json" "$target_dir/"
-
-cp -v "$chain_dir/deployed_addresses.json" "$target_dir/"
+if [ "$?" -ne "1" ]; then
+    move_over
+fi
