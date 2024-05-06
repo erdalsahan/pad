9b54ccc28f210ec7d8aaa8c8527b10e8ee974b51
Yigid BALABAN <fyb@fybx.dev>
2024-05-06 03:20:18 +0300

    enh: more tricks to bring onchain data

--- src/routes/projects/[slug]/+page.js
+++ src/routes/projects/[slug]/+page.js
@@ -4,9 +4,15 @@
  */
 
 import { error } from '@sveltejs/kit';
+import { Contract, JsonRpcProvider } from 'ethers';
+import APCampaign from '$lib/abi/APCampaign.json';
+
+// TODO this section must change on demo day!!!
+const rpcUrl = 'http://127.0.0.1:8545';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
+	// step 1: get the metadata from offchain
 	// we will mock the backend connection here, lol
 	const response = await fetch('/api/gallery.json');
 	const data = await response.json();
@@ -14,5 +20,16 @@ export async function load({ fetch, params }) {
 	const what = data.data.find((element) => element.id == params.slug);
 
 	if (what == undefined) error(404, 'Not found');
-	else return what;
+	else {
+		// step 2: the project exists, now gather onchain data
+		const provider = new JsonRpcProvider(rpcUrl);
+		const contract = new Contract(what.contractAddress, APCampaign.abi, provider);
+		const data = await contract._data();
+
+		what.parameters.contractAddress = what.contractAddress;
+		what.parameters.startTime = new Date(Number(data.saleStartTime) * 1000).toString();
+		what.parameters.endTime = new Date(Number(data.saleEndTime) * 1000);
+
+		return what;
+	}
 }
